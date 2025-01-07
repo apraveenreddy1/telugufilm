6d312b5a0e5a15d55c84b3fc899411a79ef9bee1
Praveen Reddy <praveenreddy@cmg.com.my>
2025-01-07 15:57:16 +0530

    home page changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { createUrl } from '@/core/routing/utils';
+import { getMetadata } from '@/core/seo/utils';
+import {
+  FeaturedListSectionSkeleton,
+} from '@/features/home/components/featured-list-section';
+import { MediaType } from '@/features/media/utils';
+import {
+  getMovieGenres,
+} from '@/features/movies/data';
+import { Divider, Stack } from '@mui/material';
+import { Suspense } from 'react';
 
-export default function Home() {
+export const metadata = getMetadata({
+  title: 'Home',
+  pathname: '/',
+});
+
+export default async function HomePage() {
+  const [movieGenres] = await Promise.all([
+    getMovieGenres(),
+  ]);
+
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Stack spacing={6}>
+        <Divider />
+
+        
+
+        
+
+        
+
+        {movieGenres.slice(0, 5).map((genre) => {
+          const searchParams = new URLSearchParams();
+          searchParams.set('genreId', genre.id.toString());
+
+          const title = `${genre.name} Movies`;
+          const mediaType = MediaType.MOVIE;
+
+          return (
+            <Suspense
+              key={genre.id}
+              fallback={
+                <FeaturedListSectionSkeleton
+                  title={title}
+                  mediaType={mediaType}
+                />
+              }
+            >
+        
+            </Suspense>
+          );
+        })}
+      </Stack>
+    </main>
   );
 }
