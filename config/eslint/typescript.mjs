3e5cbb3a8e83dd425dbc1cf285cdf36d91ed16fc
Praveen Reddy <praveenreddy@cmg.com.my>
2025-01-10 13:16:55 +0530

    test

--- config/eslint/typescript.mjs
+++ config/eslint/typescript.mjs
@@ -14,16 +14,8 @@ export const typescriptConfig = [
   },
   {
     rules: {
-      '@typescript-eslint/prefer-destructuring': 'warn',
-      '@typescript-eslint/consistent-type-imports': 'warn',
-      '@typescript-eslint/no-misused-promises': [
-        'warn',
-        { checksVoidReturn: false },
-      ],
-      '@typescript-eslint/restrict-template-expressions': [
-        'warn',
-        { allowNumber: true },
-      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
     },
   },
   // https://typescript-eslint.io/getting-started/typed-linting/#how-can-i-disable-type-aware-linting-for-a-subset-of-files
