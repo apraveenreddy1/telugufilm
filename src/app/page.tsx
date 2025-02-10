80d866914bc08b53f33d276b2a0163640252bbc1
Praveen Reddy <praveenreddy@cmg.com.my>
2025-02-10 17:39:11 +0530

    nodejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { createUrl } from '@/core/routing/utils';
 import { getMetadata } from '@/core/seo/utils';
 import { FIRST_PAGE } from '@/core/shared/utils';
 import {
@@ -8,12 +7,8 @@ import {
 import { MediaType } from '@/features/media/utils';
 import { FeaturedMovie } from '@/features/movies/components/featured-movie';
 import {
-  getDiscoverMovies,
-  getMovieGenres,
   getPopularMovies,
   getSliderMovies,
-  getTopRatedMovies,
-  getNewsList
 } from '@/features/movies/data';
 import { getPopularPeople } from '@/features/people/data';
 import { Divider, Stack } from '@mui/material';
@@ -25,8 +20,7 @@ export const metadata = getMetadata({
 });
 
 export default async function HomePage() {
-  const [movieGenres, popularMovies, sliders] = await Promise.all([
-    getMovieGenres(),
+  const [popularMovies, sliders] = await Promise.all([
     getPopularMovies(FIRST_PAGE),
     getSliderMovies()
   ]);
@@ -39,100 +33,22 @@ export default async function HomePage() {
 
       <Stack spacing={6}>
         <Divider />
-
-        <Suspense
-          fallback={
-            <FeaturedListSectionSkeleton
-              title="Popular Movies"
-              mediaType={MediaType.MOVIE}
-            />
-          }
-        >
-          <FeaturedListSection
-            title="Popular Movies"
-            mediaType={MediaType.MOVIE}
-            seeAllHref="/movies/popular"
-            promise={getPopularMovies(FIRST_PAGE)}
-            // Since we show the first popular movie as the featured movie,
-            // we don't show it here in the list again.
-            skipFirstItem
-          />
-        </Suspense>
-
-        <Suspense
-          fallback={
-            <FeaturedListSectionSkeleton
-              title="Top Rated Movies"
-              mediaType={MediaType.MOVIE}
-            />
-          }
-        >
-          <FeaturedListSection
-            title="Top Rated Movies"
-            mediaType={MediaType.MOVIE}
-            seeAllHref="/movies/top-rated"
-            promise={getTopRatedMovies(FIRST_PAGE)}
-          />
-        </Suspense>
-
-        <Suspense
-          fallback={
-            <FeaturedListSectionSkeleton
-              title="News"
-              mediaType={MediaType.MOVIE}
-            />
-          }
-        >
-          <FeaturedListSection
-            title="News"
-            mediaType={MediaType.MOVIE}
-            seeAllHref="/movies/news"
-            promise={getNewsList(FIRST_PAGE)}
-          />
-        </Suspense>
-
-        <Suspense
-          fallback={
-            <FeaturedListSectionSkeleton
+        
+          <Suspense
+            fallback={
+              <FeaturedListSectionSkeleton
+                title="Popular People"
+                mediaType={MediaType.PERSON}
+              />
+            }
+          >
+            <FeaturedListSection
               title="Popular People"
               mediaType={MediaType.PERSON}
+              seeAllHref="/people/popular"
+              promise={getPopularPeople(FIRST_PAGE)}
             />
-          }
-        >
-          <FeaturedListSection
-            title="Popular People"
-            mediaType={MediaType.PERSON}
-            seeAllHref="/people/popular"
-            promise={getPopularPeople(FIRST_PAGE)}
-          />
-        </Suspense>
-
-        {movieGenres.slice(0, 5).map((genre) => {
-          const searchParams = new URLSearchParams();
-          searchParams.set('genreId', genre.id.toString());
-
-          const title = `${genre.name} Movies`;
-          const mediaType = MediaType.MOVIE;
-
-          return (
-            <Suspense
-              key={genre.id}
-              fallback={
-                <FeaturedListSectionSkeleton
-                  title={title}
-                  mediaType={mediaType}
-                />
-              }
-            >
-              <FeaturedListSection
-                title={`${genre.name} Movies`}
-                mediaType={mediaType}
-                seeAllHref={createUrl('/movies/discover', searchParams)}
-                promise={getDiscoverMovies(FIRST_PAGE, genre.id)}
-              />
-            </Suspense>
-          );
-        })}
+          </Suspense>
       </Stack>
     </main>
   );
--- src/core/layouts/components/app-drawer.tsx
+++ src/core/layouts/components/app-drawer.tsx
@@ -137,56 +137,16 @@ export function AppDrawer({ genres }: AppDrawerProps) {
           overflow: 'auto',
         }}
       >
-        <List subheader={<ListSubheader>Discover</ListSubheader>}>
-          <AppDrawerItem
-            href="/movies/discover"
-            selected={pathname === '/movies/discover' && !genreId}
-            icon={<LightbulbIcon />}
-            title="Discover Movies"
-          />
-          <AppDrawerItem
-            href="/movies/popular"
-            selected={pathname === '/movies/popular'}
-            icon={<TrendingUpIcon />}
-            title="Popular Movies"
-          />
-          <AppDrawerItem
-            href="/movies/top-rated"
-            selected={pathname === '/movies/top-rated'}
-            icon={<StarIcon />}
-            title="Top Rated Movies"
-          />
-           <AppDrawerItem
-            href="/movies/news"
-            selected={pathname === '/movies/news'}
-            icon={<FeedIcon />}
-            title="News"
-          />
+        <List subheader={<ListSubheader>Discover</ListSubheader>}>          
           <AppDrawerItem
             href="/people/popular"
             selected={pathname === '/people/popular'}
             icon={<PersonIcon />}
-            title="Popular People"
+            title="Directors"
           />
         </List>
         <Divider />
-        <List subheader={<ListSubheader>Movie Genres</ListSubheader>}>
-          {genres.map((genre) => {
-            const searchParams = new URLSearchParams();
-            searchParams.set('genreId', genre.id.toString());
-
-            return (
-              <AppDrawerItem
-                key={genre.id}
-                href={createUrl('/movies/discover', searchParams)}
-                title={genre.name}
-                selected={
-                  pathname === '/movies/discover' && genreId === genre.id
-                }
-              />
-            );
-          })}
-        </List>
+        
       </Box>
       <Divider />
       <TmdbAttribution />
